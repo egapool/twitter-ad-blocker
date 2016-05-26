8a57261956499d86db1dbab17098c9429aa98cc5
egapool <egpersent@gmail.com>
2016-05-27 00:01:19 +0900

    change callback url

--- src/public/index.php
+++ src/public/index.php
@@ -74,7 +74,7 @@ $app->get('/auth/twitter', function (Request $request, Response $response) {
 	$config = [
 	    'security_salt' => $this->get('settings')['oauth']['twitter']['security_salt'], //レスポンスのシグネチャ生成に使うソルト値。デフォルトのままだと Notice が出ておこられる
 	    'path' => '/auth/', // Opauth を動かす URL のパス
-	    'callback_url' => '/auth/callback',
+	    'callback_url' => '/auth/twitter/callback',
 
 	    'Strategy' => [
 	        'Twitter' => [
@@ -88,7 +88,7 @@ $app->get('/auth/twitter', function (Request $request, Response $response) {
 
     return $response;
 });
-$app->get('/auth/twitter/oauth_callback', function (Request $request, Response $response) {
+$app->get('/auth/twitter/callback', function (Request $request, Response $response) {
 	echo '<a href="/test">TEST API</a>';
     return $response;
 })->setName('top');

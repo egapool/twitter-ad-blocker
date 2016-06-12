3374daeb73921c804aa6e0da5a627f1340435310
egapool <egpersent@gmail.com>
2016-06-12 14:58:13 +0900

    http header取得

--- src/public/index.php
+++ src/public/index.php
@@ -64,7 +64,7 @@ $container['view'] = new \Slim\Views\PhpRenderer("../templates/");
 
 
 $app->get('/agent', function (Request $request, Response $response) {
-	var_dump($_SERVER['HTTP_USER_AGENT']);die;
+	var_dump(apache_response_headers());die;
 });
 
 $app->get('/', function (Request $request, Response $response) {

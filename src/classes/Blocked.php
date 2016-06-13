91e03eb8d343c7b44b21581184a95957a9fc0464
egapool <egpersent@gmail.com>
2016-06-13 23:19:34 +0900

    ブロックした広告の表示を最新100件に

--- src/classes/Blocked.php
+++ src/classes/Blocked.php
@@ -15,6 +15,7 @@ class Blocked
 		$sql .= "inner join ad_accounts as a ON l.ad_id = a.id" . PHP_EOL;
 		$sql .= "where l.user_id = :user_id" . PHP_EOL;
 		$sql .= "order by created_at DESC" . PHP_EOL;
+		$sql .= "LIMIT 100" . PHP_EOL;
 		$sth = $this->dbh->prepare($sql);
 		$sth->bindValue(':user_id', $user_id, PDO::PARAM_STR);
 		$sth->execute();
